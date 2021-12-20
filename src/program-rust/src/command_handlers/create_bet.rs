1dae92d6d4d6fd8c98af5c6a2869152950e2ab22
Jorge Hernandez <jdaviderb@gmail.com>
2021-12-19 22:14:13 -0500

    add some validations

--- src/program-rust/src/command_handlers/create_bet.rs
+++ src/program-rust/src/command_handlers/create_bet.rs
@@ -8,17 +8,27 @@ use solana_program::{
 use borsh::{BorshDeserialize, BorshSerialize};
 use crate::accounts::{player_account, command};
 
+const MIN_LAMPORTS: u64 = 6000000;
+const VALID_BETS: [u32; 3] = [1, 2, 3]; 
+
 pub fn handler(
   _program_id: &Pubkey,
   accounts: &[AccountInfo],
   instruction_data: &[u8],
 ) -> ProgramResult {
-
   let accounts_iter = &mut accounts.iter();
   let account = next_account_info(accounts_iter)?;
 
   let mut player_account = player_account::Account::try_from_slice(&account.data.borrow())?;
   let command_data = command::Account::try_from_slice(&instruction_data)?;
+
+  if account.lamports() < MIN_LAMPORTS {
+    return Err(ProgramError::IncorrectProgramId);
+  }
+
+  if !VALID_BETS.contains(&command_data.data) {
+    return Err(ProgramError::IncorrectProgramId);
+  }
 
   if player_account.bet > 0 {
     return Err(ProgramError::IncorrectProgramId);
